df72243c8a3ded0d492e26851282376936e7b75d
Chris Elrod <elrodc@gmail.com>
2019-12-19 03:24:28 -0500

    Updated manifest and precompile.

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,5 +1,6 @@
 function _precompile_()
     ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
-    precompile(Tuple{typeof(StructuredMatrices.muladd_quote),Int64,Type,Int64,Int64,Symbol})
-    precompile(Tuple{typeof(StructuredMatrices.row_sum_add_quote),Int64,Type,Symbol})
+    precompile(Tuple{typeof(StructuredMatrices.binomial2),Int64})
+    precompile(Tuple{typeof(StructuredMatrices.muladd_quote),Int64,Type{T} where T,Int64,Int64,Symbol})
+    precompile(Tuple{typeof(StructuredMatrices.row_sum_add_quote),Int64,Type{T} where T,Symbol})
 end
